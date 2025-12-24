9cf312795555127a5ef023cd6b2512c2fa8845ea
cosmicakes <belenriquelme832@gmail.com>
2025-12-24 18:22:51 -0300

    Create Autor.java

---
+++ Autor.java
@@ -0,0 +1,32 @@
+package com.alura.literalura.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "autores")
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String nombre;
+    private Integer fechaDeNacimiento;
+    private Integer fechaDeFallecimiento;
+
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Libro> libros;
+
+    public Autor() {}
+    public Autor(DatosAutor datosAutor) {
+        this.nombre = datosAutor.nombre();
+        this.fechaDeNacimiento = datosAutor.fechaDeNacimiento();
+        this.fechaDeFallecimiento = datosAutor.fechaDeFallecimiento();
+    }
+
+    // Getters y Setters...
+    @Override
+    public String toString() {
+        return "Autor: " + nombre + " (" + fechaDeNacimiento + " - " + fechaDeFallecimiento + ")";
+    }
+}
