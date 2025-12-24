617bb5a6eb1b56d7147f80f39e1077df4615f114
cosmicakes <belenriquelme832@gmail.com>
2025-12-24 18:24:35 -0300

    Create AutorRepository.java

---
+++ AutorRepository.java
@@ -0,0 +1,13 @@
+package com.alura.literalura.repository;
+
+import com.alura.literalura.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+    @Query("SELECT a FROM Autor a WHERE a.fechaDeNacimiento <= :anio AND (a.fechaDeFallecimiento IS NULL OR a.fechaDeFallecimiento >= :anio)")
+    List<Autor> buscarAutoresVivosEnDeterminadoAnio(Integer anio);
+    
+    Autor findByNombreIgnoreCase(String nombre);
+}
